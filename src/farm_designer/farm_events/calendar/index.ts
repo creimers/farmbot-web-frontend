82ab3be37ab21c78b52d34481b615467778397c6
Rick Carlino <rick.carlino@gmail.com>
2017-06-19 16:04:49 -0500

    Almost done. Need to finish adding regimen items to calendar. WIP

--- src/farm_designer/farm_events/calendar/index.ts
+++ src/farm_designer/farm_events/calendar/index.ts
@@ -1,56 +1,37 @@
-// import { Dictionary } from "farmbot/dist";
-// import {
-//   NewCalendarItem,
-//   FarmEventWithExecutable,
-//   FarmEventWithRegimen,
-//   FarmEventWithSequence
-// } from "./interfaces";
-// import * as moment from "moment";
+import { Dictionary } from "farmbot/dist";
+import { CalendarOccurrence } from "../../interfaces";
+import * as moment from "moment";
 
-// /** A look up object for calendar items (farm events). */
-// export class Calendar {
-//   static KEY_FORMAT = "MMDD";
-//   constructor(public value: Dictionary<NewCalendarItem[]> = {}) { }
+export class Calendar {
+  static DATE_FORMAT = "MMDD";
+  static MONTHS: Readonly<Dictionary<string>> = {
+    "12": "Dec",
+    "11": "Nov",
+    "10": "Oct",
+    "09": "Sep",
+    "08": "Aug",
+    "07": "Jul",
+    "06": "Jun",
+    "05": "May",
+    "04": "Apr",
+    "03": "Mar",
+    "02": "Feb",
+    "01": "Jan"
+  };
 
-//   add = (item: FarmEventWithExecutable) => {
-//     switch (item.executable_type) {
-//       case "Sequence":
-//         this.addSequence(item);
-//         break;
-//       case "Regimen":
-//         this.addRegimen(item);
-//         break;
-//       default:
-//         throw new Error("Bad executable");
-//     }
-//     return this;
-//   }
+  constructor(public value: Dictionary<CalendarOccurrence[]> = {}) { }
 
-//   addRegimen = (item: FarmEventWithRegimen) => {
-//     // Add the start
-//     let time = moment(item.start_time);
-//     this.insert(item.start_time, {
-//       dayOfMonth: time.day(),
-//       month: time.format("MMM"),
-//       executable_id: item.executable_id,
-//       executable_type: item.executable_type,
-//       farm_event_id: item.id || -1,
-//       label: item.executable.name,
-//       sortKey: time.unix(),
-//       timeStr: time.format("hh mm dd yy")
-//     });
-//     // Add *all* the regimen items.
-//     throw new Error("TODO");
-//   }
+  insert(m: moment.Moment, occur: CalendarOccurrence) {
+    let k = this.fmtDate(m);
+    this.value[k] = this.value[k] || [];
+    this.value[k].push(occur);
+  }
 
-//   addSequence = (item: FarmEventWithSequence) => {
-//   }
+  findByDate(m: moment.Moment): CalendarOccurrence[] {
+    return this.value[this.fmtDate(m)] || [];
+  }
 
-//   insert = (date: string, item: NewCalendarItem) => {
-//     let k = this.keyFor(date);
-//     this.value[k] = this.value[k] || [];
-//     this.value[k].push(item);
-//   }
-
-//   keyFor = (jsonDate: string) => moment(jsonDate).format(Calendar.KEY_FORMAT);
-// }
+  fmtDate = (m: moment.Moment) => {
+    return m.format(Calendar.DATE_FORMAT);
+  }
+}
--- src/farm_designer/farm_events/map_state_to_props.ts
+++ src/farm_designer/farm_events/map_state_to_props.ts
@@ -1,29 +1,20 @@
 import { Everything } from "../../interfaces";
 import * as moment from "moment";
-import { Dictionary } from "farmbot";
 import { FarmEventProps, CalendarOccurrence, CalendarDay } from "../interfaces";
-import {
-  selectAllFarmEvents,
-  indexSequenceById,
-  indexRegimenById
-} from "../../resources/selectors";
 import { joinFarmEventsToExecutable } from "./calendar/selectors";
+import { Calendar } from "./calendar/index";
+import { FarmEventWithSequence, FarmEventWithExecutable } from "./calendar/interfaces";
 
-const FORMAT = "MMDD";
-const MONTHS: Readonly<Dictionary<string>> = {
-  "12": "Dec",
-  "11": "Nov",
-  "10": "Oct",
-  "09": "Sep",
-  "08": "Aug",
-  "07": "Jul",
-  "06": "Jun",
-  "05": "May",
-  "04": "Apr",
-  "03": "Mar",
-  "02": "Feb",
-  "01": "Jan"
-};
+/** Make a calendar occurence for a time and farm event. */
+function occurrence(m: moment.Moment, fe: FarmEventWithExecutable) {
+  return {
+    sortKey: m.unix(),
+    timeStr: m.format("hh:mm a"),
+    executableName: fe.executable.name || fe.executable_type,
+    executableId: fe.executable_id || 0,
+    id: fe.id || 0,
+  };
+}
 
 /** Prepares a FarmEvent[] for use with <FBSelect /> */
 export function mapStateToProps(state: Everything): FarmEventProps {
@@ -31,24 +22,22 @@ export function mapStateToProps(state: Everything): FarmEventProps {
   let x = joinFarmEventsToExecutable(state.resources.index);
 
   let push = (state && state.router && state.router.push) || (() => { });
-
-  let farmEventByMMDD: Dictionary<CalendarOccurrence[]> = x
-    .reduce(function (memo, fe) {
-      (fe.calendar || []).map(function (date) {
-        let m = moment(date);
-        let mmdd = m.format(FORMAT);
-        let executableId = fe.executable_id;
-        let occur = {
-          sortKey: m.unix(),
-          timeStr: m.format("hh:mm a"),
-          executableName: fe.executable.name || fe.executable_type,
-          executableId,
-          id: fe.id || 0,
-        };
-        (memo[mmdd]) ? memo[mmdd].push(occur) : (memo[mmdd] = [occur]);
-      });
-      return memo;
-    }, ({} as Dictionary<CalendarOccurrence[]>));
+  let calendar = new Calendar();
+  // let farmEventByMMDD: Dictionary<CalendarOccurrence[]> =
+  x.map(function (fe) {
+    if (fe.executable_type === "Regimen") {
+      fe.executable.regimen_items.map(ri => {
+        // Add the offset, give it a special name, push it into the calendar.
+        debugger;
+        console.log()
+        throw new Error("STOPPED HERE");
+      })
+    }
+    (fe.calendar || []).map(function (date) {
+      let m = moment(date);
+      calendar.insert(m, occurrence(m, fe))
+    });
+  });
 
   let calendarRows: CalendarDay[] = _.chain(x)
     .map(y => y.calendar || [])  // Grab every calendar date
@@ -56,16 +45,13 @@ export function mapStateToProps(state: Everything): FarmEventProps {
     .uniq()                      // Remove dupes
     .compact()                   // Remove empty values (to be safe)
     .map(y => moment(y))         // Convert string to moment() objects.
-    .uniq(x => x.format(FORMAT)) // Grab unique day and month combos.
+    .uniq(x => x.format(Calendar.DATE_FORMAT)) // Grab unique day and month combos.
     .map(function (m) {
-      let mmdd = m.format(FORMAT);
-      let items = farmEventByMMDD[mmdd];
-      if (!items) {
-        throw new Error("No");
-      }
+      let mmdd = m.format(Calendar.DATE_FORMAT);
+      let items = calendar.findByDate(m);
       return {
         sortKey: m.unix(),
-        month: MONTHS[mmdd.slice(0, 2)] || "???",
+        month: Calendar.MONTHS[mmdd.slice(0, 2)] || "???",
         day: parseInt(mmdd.slice(2, 4)),
         items
       };
