0277e2e70504d883eb4ac829ff989bec7f55f04d
MrChristofferson <chrisvan0848@gmail.com>
2017-06-28 18:06:35 -0500

    select additions

--- src/ui/filter_search.tsx
+++ src/ui/filter_search.tsx
@@ -1,16 +1,29 @@
 import * as React from "react";
-
+import { t } from "i18next";
 import { Button, Classes, MenuItem } from "@blueprintjs/core";
 import { ISelectItemRendererProps, Select } from "@blueprintjs/labs";
+
 import { DropDownItem } from "./fb_select";
 
 const SelectComponent = Select.ofType<DropDownItem>();
+
+type PossibleReferences =
+  | "Sequences"
+  | "Regimens"
+
+interface ParentMenu {
+  title: string;
+  value: string | number;
+  subMenus: DropDownItem[];
+  reference: PossibleReferences;
+}
 
 interface Props {
   items: DropDownItem[];
   selectedItem: DropDownItem;
   onChange: (item: DropDownItem) => void;
   placeholder?: string;
+  isASubMenu?: boolean;
 }
 
 interface State {
@@ -18,38 +31,51 @@ interface State {
   filterable?: boolean;
   minimal?: boolean;
   resetOnSelect?: boolean;
+  parentMenus: ParentMenu[];
+  subMenus: DropDownItem[];
 }
 
-export class FilterSearch extends React.Component<Props, State> {
+export class FilterSearch extends React.Component<Props, Partial<State>> {
 
   public state: State = {
     item: this.props.selectedItem,
     filterable: true,
     minimal: false,
-    resetOnSelect: false
+    resetOnSelect: false,
+    parentMenus: [],
+    subMenus: []
   };
 
   render() {
     const { item, minimal, ...flags } = this.state;
-    return (
-      <SelectComponent
-        {...flags}
-        items={this.props.items}
-        itemPredicate={this.filter}
-        itemRenderer={this.renderItem}
-        noResults={<MenuItem disabled text="No results." />}
-        onItemSelect={this.handleValueChange}
-        popoverProps={{ popoverClassName: minimal ? Classes.MINIMAL : "" }}
-      >
-        <Button
-          rightIconName="double-caret-vertical"
-          text={item ? item.label : "(No selection)"}
-        />
-      </SelectComponent>
-    );
+    let renderer = this.props.isASubMenu ? this.default : this.subMenu;
+    return <SelectComponent
+      {...flags}
+      items={this.props.items}
+      itemPredicate={this.filter}
+      itemRenderer={renderer}
+      noResults={<MenuItem disabled text="No results." />}
+      onItemSelect={this.handleValueChange}
+      popoverProps={{ popoverClassName: minimal ? Classes.MINIMAL : "" }}
+    >
+      <Button
+        rightIconName="double-caret-vertical"
+        text={item ? item.label : t("(No selection)")}
+      />
+    </SelectComponent>
   }
 
-  private renderItem(params: ISelectItemRendererProps<DropDownItem>) {
+  private subMenu(params: ISelectItemRendererProps<DropDownItem>) {
+    let { handleClick, item, index } = params;
+    return <MenuItem
+      className={"filter-search-item"}
+      key={item.label || index}
+      onClick={handleClick}
+      text={`${item.label}`}
+    />
+  }
+
+  private default(params: ISelectItemRendererProps<DropDownItem>) {
     let { handleClick, item, index } = params;
     return <MenuItem
       className={"filter-search-item"}
